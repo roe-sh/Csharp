ee8fb9a1da371d8b890290e0c4e5844c07981f7d
roe-sh <92922959+roe-sh@users.noreply.github.com>
2024-06-26 00:38:56 +0300

    task1

---
+++ 6.25.2024/task1/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name : ");
+            string myName = Console.ReadLine();
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
