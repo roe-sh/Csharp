5a5e8439d3eefa7dc4370cb3a46b76fcfd0e3b6b
Rahaf Al-Shorman <92922959+roe-sh@users.noreply.github.com>
2024-06-27 20:46:04 +0300

    Add files via upload

---
+++ 6.27.2024/task 3/task 3/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////Q1
+            //     string[] ARR = { "1", "7", "9", "45" };
+            // string[] arr2 = { "Str", "alex", "moh" };
+            // string arr3 = "the fox over lazy dog";
+
+
+            ///////////////Q2
+            //            "Tomato" is at index 0
+            //"Banana" is at index 1
+            //"Watermelon" is at index 2
+
+            /////////////////////////////////////Q2
+            //string[] favoriteFoods = { "Fries", "chicken", "fish", "Pasta", "Burger" };
+            //string[] favoriteSports = { "swimming", "Basketball", "Tennis" };
+            //string[] favoriteMovies = { "black swan", "green mile", "openhaimer", "Interstellar" };
+
+
+            //Console.WriteLine("Favorite Foods:");
+            //foreach (var food in favoriteFoods)
+            //{
+            //    Console.WriteLine(food);
+            //}
+
+            //Console.WriteLine("\nFavorite Sports:");
+            //foreach (var sport in favoriteSports)
+            //{
+            //    Console.WriteLine(sport);
+            //}
+
+            //Console.WriteLine("\nFavorite Movies:");
+            //foreach (var movie in favoriteMovies)
+            //{
+            //    Console.WriteLine(movie);
+            //}
+
+
+            //Console.WriteLine("\nFavorite Foods:");
+            //for (int i = 0; i < favoriteFoods.Length; i++)
+            //{
+            //    Console.WriteLine(favoriteFoods[i]);
+            //}
+            ///////////////////////////////////////////////////////Q3
+            //Console.WriteLine("Enter three numbers :");
+
+
+            //string input = Console.ReadLine();
+            //string[] numbersStr = input.Split(',');
+            //int sum = 0;
+
+            //foreach (string numStr in numbersStr)
+            //{
+            //    sum += int.Parse(numStr.Trim());
+            //}
+            ////////////////////////////////////////////Q4
+
+
+
+
+
+
+
+
+
+            /////////////////////////////////////////////////////////Q5
+            //int n = 20; 
+            //int sum = 0;
+            //int count = 0;
+
+            //Console.WriteLine($"The first {n} odd numbers:");
+
+            //for (int i = 1; i <= 100; i += 2)
+            //{
+            //    Console.Write(i + " ");
+            //    sum += i;
+            //    count++;
+
+            //    if (count == n)
+            //        break;
+            /////////////////////////////////////////////////Q6
+
+            //int n = 4;
+
+
+            //for (int i = 1; i <= n; i++)
+            //{
+
+            //    for (int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+
+            //    Console.WriteLine();
+
+
+            //}
+        }
+    }
+    //////////////////////////////////////////////////////Q7
+   
+        }
+    
+
